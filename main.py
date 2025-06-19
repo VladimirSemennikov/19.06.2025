45b85b74cc3890a71e2f5356a7681ede8b1aabd8
VladimirSemennikov <mectb1992@gmail.com>
2025-06-19 21:10:06 +0400

    komit

--- main.py
+++ main.py
@@ -88,16 +88,128 @@ super().атрибут
 """
 Важные моменты
 1) в команде super().sum_number(x,y) мы должны обязательно указать параметры x,y
-потому что
 """
 
-class Alfa():
-    @staticmethod
-    def sum_numbers(x, y):
-        return x + y
-class Beta(Alfa):
-    def result(self, x, y, z):
-        summa = super().sum_numbers(x, y)
-        print(summa / z)
-test = Beta()
-test.result(10,20,30)
+# class Alfa():
+#     @staticmethod
+#     def sum_numbers(x, y):
+#         return x + y
+# class Beta(Alfa):
+#     def result(self, x, y, z):
+#         summa = super().sum_numbers(x, y)
+#         print(summa / z)
+# test = Beta()
+# test.result(10,20,30)
+
+
+# class Person:
+#     def __init__(self, name, age, height, weight):          #создает 4 атрибута
+#         self.name = name
+#         self.age = age
+#         self.height = height
+#         self.weight = weight
+# class Student(Person):                  #студент
+#     def __init__(self, name, age, height, weight, class_number, hobby):
+#         super().__init__(name, age, height, weight)                     #запускаем инит родитеслького класса
+#         self.class_number = class_number
+#         self.hobby = hobby
+# class Worker(Person):                       #сотрудники
+#     def __init__(self, name, age, height, weight, work, experience):
+#         super().__init__(name, age, height, weight)
+#         self.work = work
+#         self.experience = experience
+# id1 = Student('Tanya', 6, 100, 25, 1, 'cook')
+# id2 = Worker('Vasya', 28, 180, 70, 'driver', 6)
+#
+# print(id1.__dict__)
+# print(id2.__dict__)
+
+#1 способ
+# class Parent:
+#     def grout(self):
+#         print("Hello from Parent")
+# class Child(Parent):
+#     def grout(self):
+#         super().grout()
+#         print("Hello from Child")
+# c = Child()
+# c.grout()
+
+#2 способ
+# class Parent:
+#     def grout(self):
+#         print("Hello from Parent")
+# class Child(Parent):
+#     def grout(self):
+#         super(Child, self).grout()
+#         print("Hello from Child")
+# c = Child()
+# c.grout()
+
+
+#Множественные наследования
+
+# class A:
+#     def method1(self):
+#         print("Method 1 from A")
+# class B:
+#     def method2(self):
+#         print("Method 2 from B")
+# class C(A, B):
+#     def method3(self):
+#         print("Method 3 from C")
+# child = C()
+#
+# child.method1()     #Method 1 from A
+# child.method2()     #Method 2 from B
+# child.method3()     #Method 3 from C
+
+
+
+# class King:
+#     a = 4
+# class Queen:
+#     b = 6
+# class Prince(King, Queen):
+#     c = 10
+#
+# count = Prince()
+# print(f"Результат сложения: {count.a + count.b + count.c}")
+
+
+#Пример влияния порядка наследования
+
+# class A:
+#     def __init__(self):
+#         print("Метод класса А")
+# class B:
+#     def __init__(self):
+#         print("Метод класса В")
+# class C(A, B):
+#     def __init__(self):
+#         print("Метод класса С")
+#
+# c = C()
+
+"""
+Атрибут __mro__
+MRO - (method resolution order) - это порядокв котором Python ищет методы в иерархии
+класса при наследовании. (С3 Linearization) для определения MRO. 
+"""
+
+# class A:
+#     pass
+# class B(A):
+#     pass
+# class C(A):
+#     pass
+# class D(B, C):
+#     pass
+# print(D.__mro__)
+#(<class '__main__.D'>, <class '__main__.B'>, <class '__main__.C'>, <class '__main__.A'>, <class 'object'>)
+
+"""
+MRO особенно полезен при множественном наследовании. когда у класса есть несколько родительских классов.
+Он помогает избежать проблем с дублированием методов и позволяет определить, какой метод будет вызван
+в случае конфликта имен методов а разных классах
+"""
